1a70d5142243458e38114e95cf70651aaa2f345e
Jack Red <beardynamics689@gmail.com>
2023-11-19 22:19:54 -0500

    Nightly

---
+++ main.py
@@ -0,0 +1,34 @@
+import turtle
+import numpy
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+s.delay(1 / 60)
+t.hideturtle()
+
+points = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+]
+
+pDistX = s.window_width / 12
+pDistY = s.window_height / 12
+
+a = 1
+
+def updateScreen():
+    pass
+
+while 1:
+    updateScreen()
+
+    s.clearscreen()
